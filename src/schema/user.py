5dd825d8b76801285672282c12d81f856f6b555c
successdevop <successraphael28@gmail.com>
2026-05-18 11:28:05 +0100

    implemented the user schema for users

---
+++ src/schema/user.py
@@ -0,0 +1,11 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from src.model.user import User
+
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        fields = ("user_id", "user_name", "email", "password")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)

98768b359b66cdb928d7e3c597bdf91ac9324088
successdevop <successraphael28@gmail.com>
2026-05-19 10:34:49 +0100

    implemented the post controller for post service

---
+++ src/controllers/post_controller.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from src.services.post_service import PostService
+from src.repo.database import Database
+from flask_jwt_extended import jwt_required
+
+
+db = Database().db
+post_service = PostService(database=db)
+
+post_bp = Blueprint("main", __name__)
+
+@post_bp.route("/post", methods=["POST"])
+@jwt_required()
+def create_post():
+    return post_service.create_post()
+

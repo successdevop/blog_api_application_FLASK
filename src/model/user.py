07899e387ede7260abd80912163a77c33d713c5d
successdevop <successraphael28@gmail.com>
2026-05-20 14:56:50 +0100

    remodified the user model

--- src/model/user.py
+++ src/model/user.py
@@ -1,16 +1,14 @@
 from sqlalchemy.orm import relationship
 from src.repo.database import Database
-from sqlalchemy import Column, Integer, String
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token
+from sqlalchemy import Column, String
 
 
 class User(Database.db.Model):
     __tablename__ = "users"
-    user_id = Column(String, unique=True, primary_key=True)
+    user_id = Column(String, primary_key=True)
     user_name = Column(String(64), nullable=False, unique=True, index=True)
     email = Column(String(120), nullable=False, unique=True, index=True)
-    password = Column(String(20), nullable=False)
+    password = Column(String(225), nullable=False)
     posts = relationship("Post", back_populates="user", lazy="dynamic")
     comments = relationship("Comments", back_populates="user", lazy="dynamic")
 

1dd56472bf598aafeb2fcf87c6a8cce9f5467f3d
successdevop <successraphael28@gmail.com>
2026-05-20 14:52:54 +0100

    implemented the set_password method, check_password method and generate_user_token method to make our application easy and beautiful

--- src/model/user.py
+++ src/model/user.py
@@ -7,27 +7,13 @@ from flask_jwt_extended import create_access_token
 
 class User(Database.db.Model):
     __tablename__ = "users"
-    user_id = Column(Integer, unique=True, primary_key=True)
+    user_id = Column(String, unique=True, primary_key=True)
     user_name = Column(String(64), nullable=False, unique=True, index=True)
     email = Column(String(120), nullable=False, unique=True, index=True)
     password = Column(String(20), nullable=False)
     posts = relationship("Post", back_populates="user", lazy="dynamic")
     comments = relationship("Comments", back_populates="user", lazy="dynamic")
 
-    def set_password(self, password: str):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password: str):
-        return check_password_hash(self.password, password)
-
-    @classmethod
-    def authenticate_user(cls, email: str, password: str):
-        user = cls.query.filter_by(email=email).first()
-
-        if user and user.check_password(password=password):
-            access_token = create_access_token(identity=str(user.user_id))
-            return access_token, user
-        return None
 
     def __repr__(self):
         return f"User(user_id:{self.user_id} | user_name:{self.user_name}"
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,29 @@
 from flask import jsonify
+from typing import Type
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def server_error(error):
     return jsonify({"error": str(error)}), 500
 
+
 def status_msg(message: str, status_code: int = 401):
     return jsonify({"message":message}), status_code
+
+
+def set_password(obj: Type, password: str):
+    obj.password = generate_password_hash(password=password)
+
+
+def check_password(obj: Type, password: str):
+    return check_password_hash(obj.password, password=password)
+
+
+def generate_user_token(obj: Type, email: str, password: str):
+    user = obj.query.filter_by(email=email).first()
+
+    if user and check_password(obj, password=password):
+        access_token = create_access_token(identity=obj.user_id)
+        return access_token, user
+    return None

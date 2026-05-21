aedc5230cfdca6389d51c99be9e2839d4a3a6162
successdevop <successraphael28@gmail.com>
2026-05-21 15:25:06 +0100

    implemented the verify_email and verify_password methods

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import request
 from src.model.user import User
 from src.utils.utils import server_error, status_msg, set_password, generate_user_token
@@ -7,20 +9,45 @@ class Auth:
     def __init__(self, database=None):
         self._database = database
 
+    def _validate_email(self, email: str):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return bool(re.match(pattern, email))
+
+    def _validate_password(self, password: str):
+        if len(password) < 8:
+            return False, "Password must be at least 8 characters"
+        if not re.search(r'[A-Z]', password):
+            return False, "Password must contain at least one upper case letter"
+        if not re.search(r'[0-9]', password):
+            return False, "Password must contain at least one number"
+
+        return True, "OK"
+
     def register(self):
         data = request.get_json()
         if not data:
-            return status_msg("Invalid or missing data")
+            return status_msg("Invalid or missing data", 400)
 
-        user_name = data.get("user_name")
-        email = data.get("email")
-        password = data.get("password")
+        user_name = data.get("user_name", "").strip()
+        email = data.get("email", "").strip().lower()
+        password = data.get("password", "").strip()
 
-        if not user_name or not email or not password:
-            return status_msg("Incomplete or missing credentials")
+        if not user_name or len(user_name) < 3:
+            return status_msg("user_name must be at least 3 characters", 400)
+
+        is_valid_email = self._validate_email(email)
+        if not is_valid_email:
+            return status_msg(f"Email format is invalid", 400)
 
         if User.query.filter_by(email=email).first():
             return status_msg("Email already exists", status_code=409)
+
+        if User.query.filter_by(user_name=user_name).first():
+            return status_msg("user_name already exists", 409)
+
+        is_valid_password, msg = self._validate_password(password=password)
+        if not is_valid_password:
+            return status_msg(msg, 400)
 
         new_user = User(user_name=user_name, email=email)
         set_password(new_user, password)
@@ -28,7 +55,11 @@ class Auth:
         try:
             self._database.session.add(new_user)
             self._database.session.commit()
-            return status_msg(f"Congratulations {user_name}, your registration is successful", status_code=201)
+            return status_msg({
+                "message":"Registration successful",
+                "user_id":new_user.user_id,
+                "user_name":new_user.user_name
+            }, status_code=201)
 
         except Exception as e:
             self._database.session.rollback()

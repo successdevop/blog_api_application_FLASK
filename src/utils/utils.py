22d07c29e7422b5557f9091e057b9fbd2298df39
successdevop <successraphael28@gmail.com>
2026-05-21 11:29:55 +0100

    implemented the pagination method for easy navigation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from src.model.user import User
 from typing import Type
 from flask_jwt_extended import create_access_token
@@ -20,6 +20,7 @@ def set_password(obj: User, password: str):
 def check_password(obj: User, password: str):
     return check_password_hash(obj.password, password=password)
 
+
 def generate_user_token(obj: Type[User], email: str, password: str):
     user = obj.query.filter_by(email=email).first()
 
@@ -27,3 +28,17 @@ def generate_user_token(obj: Type[User], email: str, password: str):
         access_token = create_access_token(identity=user.user_id)
         return access_token, user
     return None
+
+
+def pagination(obj: Type):
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per_page", 20, type=int)
+
+    per_page = min(per_page, 100)
+
+    paginated = obj.query.paginate(
+        page=page,
+        per_page=per_page,
+        error_out=False
+    )
+    return paginated, page, per_page
